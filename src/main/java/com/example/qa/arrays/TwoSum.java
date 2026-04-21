5ed07af105c2ef1b412223f3d6d57a47051a1916
amir rashidi <amirrashidihamzah91@yahoo.com>
2026-04-21 17:15:13 +0800

    two sum problem

--- src/main/java/com/example/qa/arrays/TwoSum.java
+++ src/main/java/com/example/qa/arrays/TwoSum.java
@@ -1,2 +1,26 @@
-package com.example.qa.arrays;public class TwoSum {
+package com.example.qa.arrays;
+
+import java.util.HashMap;
+
+//Question
+//Write a Java program to find two numbers in an array whose sum equals a given target.
+public class TwoSum {
+
+    public static void main(String[] args){
+
+        int[] arr = {2, 7, 11, 15};
+        int target = 9;
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i= 0;i< arr.length;i++){
+            int complement = target - arr[i];
+
+            if(map.containsKey(complement)){
+                System.out.println("Pair Found:" + arr[i] + " + " + complement);
+                break;
+            }
+            map.put(arr[i],i);
+        }
+    }
 }
--- src/main/java/com/example/qa/arrays/TwoSumUsingHashSet.java
+++ src/main/java/com/example/qa/arrays/TwoSumUsingHashSet.java
@@ -1,2 +1,23 @@
-package com.example.qa.arrays;public class TwoSumUsingHashSet {
+package com.example.qa.arrays;
+
+import java.util.HashSet;
+
+public class TwoSumUsingHashSet {
+
+    public static void main(String[] args){
+
+        int[] arr = {2, 7, 11, 15};
+        int target = 9;
+
+        HashSet<Integer>set=new HashSet<>();
+
+        for(int num:arr){
+            int needed = target-num;
+            if(set.contains(needed)){
+                System.out.println(num + " + " + needed);
+                break;
+            }
+            set.add(num);
+        }
+    }
 }
